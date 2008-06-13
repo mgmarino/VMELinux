902cb2a350d788e16cf845164c3859d34ac33f81
Mark Howe <mhowe@physics.unc.edu>
2008-06-13 18:29:39 +0000

    updating detOS.sh to query /etc/issue

--- driver/detOS.sh
+++ driver/detOS.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
-CURRENTOS=`uname -r | awk -F'.' '{print $NF}'`
+ISSUEFILE=/etc/issue
 
-if echo "$CURRENTOS" | grep "fc" > /dev/null ; then
+if grep "Fedora" ${ISSUEFILE} > /dev/null ; then
   echo "rh"
-elif echo "$CURRENTOS" | grep "rh" > /dev/null ; then
+elif grep "Red Hat" ${ISSUEFILE} > /dev/null ; then
   echo "rh"
-elif echo "$CURRENTOS" | grep "crux" > /dev/null; then
+elif grep "CRUX" ${ISSUEFILE} > /dev/null ; then
   echo "crux"
 fi
+#CURRENTOS=`uname -r | awk -F'.' '{print $NF}'`
+#
+#if echo "$CURRENTOS" | grep "fc" > /dev/null ; then
+#  echo "rh"
+#elif echo "$CURRENTOS" | grep "rh" > /dev/null ; then
+#  echo "rh"
+#elif echo "$CURRENTOS" | grep "crux" > /dev/null; then
+#  echo "crux"
+#fi
